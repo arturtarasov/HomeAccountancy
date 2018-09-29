c4b316a7c732d381f6ca658158b2cb417c0724f9
IMATIC\tarasov.a <tarasov.a@imatic.pro>
2018-09-29 11:50:18 +0300

    add a planning page

--- src/app/system/planning-page/planning-page.component.ts
+++ src/app/system/planning-page/planning-page.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {Component , OnDestroy , OnInit} from '@angular/core';
+import {BillService} from "../shared/services/bill.service";
+import {CategoriesService} from "../shared/services/categories.service";
+import {EventsService} from "../shared/services/events.service";
+import {Observable , Subscription} from "rxjs/Rx";
+import {Category} from "../shared/models/category.model";
+import {Bill} from "../shared/models/bill.model";
+import {WFMEvent} from "../shared/models/event.model";
 
 @Component({
   selector: 'wfm-planning-page',
   templateUrl: './planning-page.component.html',
   styleUrls: ['./planning-page.component.scss']
 })
-export class PlanningPageComponent implements OnInit {
+export class PlanningPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  sub1: Subscription;
+  isLoaded = false;
+  bill: Bill;
+  category: Category[] = [];
+  events: WFMEvent[] = [];
+
+  constructor(
+    private billService: BillService,
+    private categoryService: CategoriesService,
+    private evetntsService: EventsService
+  ) { }
 
   ngOnInit() {
+    Observable.combineLatest(
+      this.billService.getBill(),
+      this.categoryService.getCategories(),
+      this.evetntsService.getEvents()
+    ).subscribe((data: [Bill, Category, WFMEvent]) => {
+      this.bill = data[0];
+      this.category = data[1];
+      this.events = data[2];
+
+      this.isLoaded = true;
+    });
+  }
+
+  getCategoryCost(cat: Category): number {
+    const catEvents = this.events.filter(e => e.category === cat.id && e.type === 'outcome');
+    return catEvents.reduce((total, event) => {
+      total += event.amount;
+      return total;
+    }, 0);
+  }
+
+  private getPercent(cat: Category): number{
+    const percent = (100 * this.getCategoryCost(cat)) / cat.capacity;
+    return percent > 100 ? 100 : percent;
+  }
+
+  getCatPercent(cat: Category): string {
+    return this.getPercent(cat) + '%';
+  }
+
+  getCatColoClass(cat: Category): string {
+    const percent = this.getPercent(cat);
+    return percent < 60 ? 'success' : percent >= 100 ? 'danger' : 'warning';
+  }
+
+  ngOnDestroy() {
+    if (this.sub1) {
+      this.sub1.unsubscribe();
+    }
+
   }
 
 }
--- src/app/system/shared/services/events.service.ts
+++ src/app/system/shared/services/events.service.ts
@@ -13,4 +13,8 @@ export class EventsService extends BaseApi {
   addEvent(event: WFMEvent): Observable<WFMEvent> {
     return this.post('events', event);
   }
+
+  getEvents(): Observable<WFMEvent> {
+    return this.get('events');
+  }
 }
