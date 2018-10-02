eeba9086e050588ab7341e159e7b0582a9cd7302
IMATIC\tarasov.a <tarasov.a@imatic.pro>
2018-10-02 19:05:35 +0300

    fix bags

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -10,16 +10,16 @@ import { RecordsPageComponent } from './records-page/records-page.component';
 import { SystemComponent } from './system.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import {DropdownDirective} from "./shared/directives/dropdown.directive";
+import {DropdownDirective} from './shared/directives/dropdown.directive';
 import { BillCardComponent } from './bill-page/bill-card/bill-card.component';
 import { CurrencyCardComponent } from './bill-page/currency-card/currency-card.component';
-import {BillService} from "./shared/services/bill.service";
-import {MomentPipe} from "./shared/pipes/moment.pipe";
+import {BillService} from './shared/services/bill.service';
+import {MomentPipe} from './shared/pipes/moment.pipe';
 import { AddCategoryComponent } from './records-page/add-category/add-category.component';
 import { EditCategoryComponent } from './records-page/edit-category/edit-category.component';
 import { AddEventComponent } from './records-page/add-event/add-event.component';
-import {CategoriesService} from "./shared/services/categories.service";
-import {EventsService} from "./shared/services/events.service";
+import {CategoriesService} from './shared/services/categories.service';
+import {EventsService} from './shared/services/events.service';
 
 @NgModule({
   imports: [
